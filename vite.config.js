33b145a040340636a22a92e3da94668a862ae3a6
kudith <ginanjar.aditya15@gmail.com>
2024-07-09 19:05:48 +0700

    chore: Update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default defineConfig({
   define: {
-    'process.env': process.env,
-  },
+    'process.env': {
+      VITE_API_KEY: process.env.VITE_API_KEY
+    }
+  }
 });
